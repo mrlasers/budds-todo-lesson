99fc36a9f4cdcdb4c3d8e20450d2f14099f90249
Timothy Pew <mrlasers@mrlasers.com>
2022-08-26 12:03:47 -0700

    end of session 8/26

---
+++ todo/app.js
@@ -0,0 +1,9 @@
+const appElement = document.createElement('div')
+
+appElement.id = 'app'
+
+document.body.append(appElement)
+
+const formEl = document.createElement('form')
+
+const inputEl = document.createElement('input')
